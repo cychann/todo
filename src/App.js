6eb165645d715fe14b0bd266be69bf95f748b77a
cychann <skcbdlhc129@naver.com>
2023-01-01 19:02:14 +0900

    Refactor: 안쓰는 코드 제거

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import Header from "./components/Header/Header";
 import TodoList from "./components/TodoList/TodoList";
 import styles from "./App.module.css";
 import AddTodoForm from "./components/AddTodoForm.jsx/AddTodoForm";
-import { DarkModeContext } from "./provider/DarkModeProvider";
-import { useContext } from "react";
 
 function App() {
-  const { darkMode } = useContext(DarkModeContext);
   return (
-    <div className={`${styles.app} ${darkMode ? styles.dark : styles.light}`}>
+    <div className={styles.app}>
       <Header />
       <TodoList />
       <AddTodoForm />
